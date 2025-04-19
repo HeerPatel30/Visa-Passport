f4102b55783128c2771c514e138bb43bc8acce8a
Heer Patel <108482463+HeerPatel30@users.noreply.github.com>
2025-04-19 19:56:21 +0530

    completed the basic homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Card from './Component/VisaCards/Card';
 import Services from './Component/Services/Services';
 import Country from './Component/Country/Country';
 import Footer from './Component/Footer/Footer';
+import About from './Component/Aboutus/About';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       {/* Main Content */}
       <main className="pt-20 px-4 sm:px-6 md:px-8 space-y-6">
         <Banner />
+        <About/>
         <Card />
         <Services />
         <Country/>
